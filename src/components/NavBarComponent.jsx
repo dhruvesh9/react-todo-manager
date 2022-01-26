ff00cb679fcb66d79a5cdd2537d64289e9f8fca1
Dhruvesh Navghare <dhruvesh.navghare@cerillion.com>
2022-01-26 19:54:31 +0530

    Fixed further href related issues.

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 export default class NavBarComponent extends React.Component {
 
-    foo(){
-        console.log('Login link')
-    }
-
     render() {
         return (
             <nav className="navbar">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <a className="navbar-brand"><p>TODO MANAGER</p></a>
+                        <a className="navbar-brand" href="/#"><p>TODO MANAGER</p></a>
                     </div>
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav navbar-right">
-                            <li className="todo-font"><button onClick={this.foo}>Login</button></li>
+                            <li className="todo-font"><a href="/#">Login</a></li>
                         </ul>
                     </div>
                 </div>
